76e01a6f1ae0fc29b017c2551ca39b775bb6c3d3
mifshs <razick.akk@mail.ru>
2026-03-04 14:59:21 +0500

    salam popolam

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->with('success', 'User created successfully!');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/index', function () {
+    return view('index');
 });
