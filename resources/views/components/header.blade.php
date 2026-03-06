07faba52989fb60690cce90543a581d143b7eff9
nikitshs <pnik244@gmail.com>
2026-03-06 18:05:13 +0500

    Сделал адаптив хедера почти

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
 <header class="pt-3 pb-3">
     <div class="container">
         <div class="">
-            <div class="up-header flex grey500 justify-between text-xs mb-2 border-b-1 border-gray-300 pb-3">
+            <div class="up-header flex grey500 justify-between text-xs mb-2 border-b-1 border-gray-300 pb-3 max-md:hidden">
                 <div class="dropdown-menu flex items-center  grid-cols-3 gap-x-2">
                     <p class=" grey400">Ваш город:</p>
                     <select name="brand" 
@@ -28,7 +28,7 @@
             <div class="bottom-header flex justify-between items-center">
                 <img src="{{ asset('storage/images/logo.png') }}" alt="logo" class="h-[31px]">
 
-                <div class="flex ">
+                <div class="flex max-md:hidden">
                     <button class="bg-[#0075B1] w-[103px] h-[36px] rounded-[6px] flex items-center content-center p-[13px] mr-2 hover:bg-[#006598] transition duration-150 ease-in-out active:scale-95 cursor-pointer">
                         <img src="{{ asset('storage/images/burger.png') }}" alt="burger" class="">
                         <p class="text-sm text-white mb-[1px] ml-[10.5px]">Каталог</p>
@@ -37,7 +37,7 @@
                      hover:border-blue-400 hover:shadow-sm hover:shadow-blue-100
                      focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-200 focus-within:shadow-md
                      active:border-blue-600 active:shadow-inner
-                    transition-all duration-200 group">
+                    transition-all duration-200 group max-lg:w-[250px]">
     
    
                     <img src="{{ asset('storage/images/magnifier.png') }}" alt="magnifier" 
@@ -50,14 +50,45 @@
                     class="ml-[10px] w-full outline-none text-gray-500 bg-transparent
                      placeholder:text-gray-400 group-hover:placeholder:text-gray-500 
                      group-focus-within:placeholder:text-gray-600
-                    transition-colors duration-200">
+                    transition-colors duration-200 ">
                     </div>
                 </div>
-                <div class="flex">
+                <div class="flex max-md:hidden">
                     <img src="{{ asset('storage/images/basket.png') }}" alt="basket" class="ml-1 hover:scale-105 active:scale-95 cursor-pointer">
                     <img src="{{ asset('storage/images/user.png') }}" alt="user" class="hover:scale-105 active:scale-95 cursor-pointer">
                 </div>
-            </div>
+
+
+
+
+                <div class="md:hidden relative z-50">
+  
+    <input type="checkbox" id="menu-toggle" class="peer hidden" />
+    
+    <label for="menu-toggle" class="cursor-pointer">
+        <img src="{{ asset('storage/images/mobileBurger.png') }}" alt="burger">
+    </label>
+    
+    
+        <div class="absolute left-0 right-0 top-full bg-white border-t shadow-lg 
+                md:hidden peer-checked:block hidden z-50">
+        <div class="px-4 py-2 space-y-2">
+            <a href="#" class="block py-3 text-gray-700 hover:text-blue-600 transition border-b border-gray-100 last:border-0">Акции</a>
+            <a href="#" class="block py-3 text-gray-700 hover:text-blue-600 transition border-b border-gray-100 last:border-0">О компании</a>
+            <a href="#" class="block py-3 text-gray-700 hover:text-blue-600 transition border-b border-gray-100 last:border-0">Адреса</a>
+            <a href="#" class="block py-3 text-gray-700 hover:text-blue-600 transition border-b border-gray-100 last:border-0">Контакты</a>
+        </div>
+    </div>
+
+
+
+
+            
+
+
+
+
+
         </div>
     </div>
 </header>
