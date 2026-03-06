a80d343b13f9ccc73ed15f9e06d276ff4275f0ec
mifshs <razick.akk@mail.ru>
2026-03-06 17:25:54 +0500

    исправил ошибки

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,42 +10,6 @@
     <title>ИНТЕЛЛЕКТ-АВТО</title>
 </head>
 <body>
-
-    <x-header>
-    </x-header>
-
-    <div class="bg-[#F1F1F1]">
-        <div class="pt-5 container grid grid-cols-2">
-
-            <div class="flex flex-col items-start">
-                <div>
-                    <span class="grey500 text-[12px] font-normal">Главная / Каталог товаров</span>
-                </div>
-                <div class="mt-2 flex flex-col items-center justify-center">
-                    <span class="text-[24px] text-black font-medium">Каталог товаров</span>
-                    <div>
-                        <input type="range" min="100" max="1000000" value="1000" step="100">
-                    </div>
-                    <div class="flex items-center justify-center">
-                        <input type="text" value="Input ₽">
-                        <input type="text" value="Input ₽">
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex items-start">
-                <div class="grid grid-cols-4">
-                    <div>
-                        @for ($i = 0; $i < 12; $i++)
-                            <x-card></x-card>
-                        @endfor
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-    </div>
    
 </body>
 </html>
